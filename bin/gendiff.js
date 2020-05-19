798a4e42dd18e0a15f7ce6c68f1a1b4ab5923f21
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-05-19 16:12:19 -0700

    refactor engine

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+import program from 'commander';
 import gendiff from '../src/index.js';
 
-gendiff();
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .action(function (filepath1, filepath2) {
+    return console.log(gendiff(filepath1, filepath2));
+  })
+  .option('-f, --format [type]', 'output format');
+
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import program from 'commander';
 import fs from 'fs';
 import _ from 'lodash';
 
 const getContent = (filename) => {
   const cwd = process.cwd();
+  console.log(cwd);
   return JSON.parse(fs.readFileSync(`${cwd}/src/${filename}`, (err) => {
     return console.log(`Error: ${err}`);
   }));
 };
 
-program
-  .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
-  .action(function (fpath1, fpath2) {
-    const filepath1 = getContent(fpath1);
-    console.log(filepath1);
-    const filepath2 = getContent(fpath2);
-    console.log(filepath2);
-  })
-  .option('-f, --format [type]', 'output format');
+const gendiff = (filepath1, filepath2) => {
+  const obj1 = getContent(filepath1);
+  const obj2 = getContent(filepath2);
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const keys = _.union(keys1, keys2);
+  console.log(keys);
+  const result = {};
+  
+  return console.log(obj1, obj2);
+}
 
-export default () => program.parse(process.argv);
+export default gendiff;
