c68e860712d3a874a0e8ab91d7fcec8769e779fc
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-05-18 22:19:50 -0700

    add readfiles from filepaths feature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import program from 'commander';
+import fs from 'fs';
+
+const getContent = (filename) => {
+  const cwd = process.cwd();
+  return JSON.parse(fs.readFileSync(`${cwd}/src/${filename}`, (err) => {
+    return console.log(`Error: ${err}`);
+  }));
+};
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .action(function (fpath1, fpath2) {
+    const filepath1 = getContent(fpath1);
+    console.log(filepath1);
+    const filepath2 = getContent(fpath2);
+    console.log(filepath2);
+  })
   .option('-f, --format [type]', 'output format');
 
 export default () => program.parse(process.argv);
