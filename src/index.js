31ea5319ea01a569e5f4f56dead4e68f778aba1a
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-05-19 17:37:47 -0700

    feature: show diff for flat json files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 
 const getContent = (filename) => {
   const cwd = process.cwd();
-  console.log(cwd);
   return JSON.parse(fs.readFileSync(`${cwd}/src/${filename}`, (err) => {
     return console.log(`Error: ${err}`);
   }));
@@ -15,10 +14,28 @@ const gendiff = (filepath1, filepath2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2);
-  console.log(keys);
-  const result = {};
-  
-  return console.log(obj1, obj2);
+  const paddingAdded = '+ ';
+  const paddingRemoved = '- ';
+  const paddingSame = '  ';
+  let result = '';
+  for (let key of keys) {
+    if (keys1.includes(key) && keys2.includes(key)) {
+      if (obj1[key] === obj2[key]) {
+        result += `${paddingSame}${key}: ${obj1[key]}\n`;
+      } else {
+        result += `${paddingRemoved}${key}: ${obj1[key]}\n`;
+        result += `${paddingAdded}${key}: ${obj2[key]}\n`;
+      }
+    } else if (keys1.includes(key) && !keys2.includes(key)) {
+      result += `${paddingRemoved}${key}: ${obj1[key]}\n`;
+    } else if (!keys1.includes(key) && keys2.includes(key)) {
+      result += `${paddingAdded}${key}: ${obj2[key]}\n`;
+    } else {
+      throw new Error('Key must be found in at least one object');
+    }
+  }
+  result = `{\n${result}}`;
+  return result;
 }
 
 export default gendiff;
