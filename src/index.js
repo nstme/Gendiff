316da5c22555a472b8c158403ebdf091f6403c16
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-05-20 23:19:03 -0700

    refactor gendiff

--- src/index.js
+++ src/index.js
@@ -15,22 +15,19 @@ const gendiff = (filepath1, filepath2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2);
-  const paddingAdded = '+ ';
-  const paddingRemoved = '- ';
-  const paddingSame = '  ';
   let result = '';
   for (let key of keys) {
     if (keys1.includes(key) && keys2.includes(key)) {
       if (obj1[key] === obj2[key]) {
-        result += `${paddingSame}${key}: ${obj1[key]}\n`;
+        result += `  ${key}: ${obj1[key]}\n`;
       } else {
-        result += `${paddingRemoved}${key}: ${obj1[key]}\n`;
-        result += `${paddingAdded}${key}: ${obj2[key]}\n`;
+        result += `- ${key}: ${obj1[key]}\n`;
+        result += `+ ${key}: ${obj2[key]}\n`;
       }
     } else if (keys1.includes(key) && !keys2.includes(key)) {
-      result += `${paddingRemoved}${key}: ${obj1[key]}\n`;
+      result += `- ${key}: ${obj1[key]}\n`;
     } else if (!keys1.includes(key) && keys2.includes(key)) {
-      result += `${paddingAdded}${key}: ${obj2[key]}\n`;
+      result += `+ ${key}: ${obj2[key]}\n`;
     } else {
       throw new Error('Key must be found in at least one object');
     }
