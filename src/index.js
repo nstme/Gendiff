a4b955d2f31faa10d488400d3b53084b12d96c27
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-05-26 23:06:57 -0700

    refactor gendiff

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ const getContent = (filePath) => {
   return JSON.parse(fs.readFileSync(fullPath, (err) => console.log(`Error: ${err}`)));
 };
 
-const gendiff = (filepath1, filepath2) => {
-  const obj1 = getContent(filepath1);
-  const obj2 = getContent(filepath2);
+const getDiff = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2);
-  const result = keys.reduce((acc, key) => {
+  return `{\n${keys.reduce((acc, key) => {
     let acumulator = acc;
     if (keys1.includes(key) && keys2.includes(key)) {
       if (obj1[key] === obj2[key]) {
@@ -33,8 +31,13 @@ const gendiff = (filepath1, filepath2) => {
       return acumulator;
     }
     throw new Error('Key must be found at least in one object');
-  }, '');
-  return `{\n${result}}`;
+  }, '')}}`;
+};
+
+const gendiff = (filepath1, filepath2) => {
+  const fileData1 = getContent(filepath1);
+  const fileData2 = getContent(filepath2);
+  return getDiff(fileData1, fileData2);
 };
 
 export default gendiff;
