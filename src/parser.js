6e0d0618b73e3dc0b08a2fc23a436ed039253d4b
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-05-27 21:47:31 -0700

    feature yaml parsing

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,18 @@
 import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml';
 
 const getContent = (filePath) => {
+  const fileExtension = filePath.split('.').pop().toLowerCase();
   const fullPath = path.resolve(process.cwd(), filePath);
-  return JSON.parse(fs.readFileSync(fullPath, (err) => console.log(`Error: ${err}`)));
+  switch (fileExtension) {
+    case 'json':
+      return JSON.parse(fs.readFileSync(fullPath, 'utf-8'));
+    case 'yaml':
+      return yaml.safeLoad(fs.readFileSync(fullPath, 'utf-8'));
+    default:
+      throw new Error(`Error! File format '${fileExtension}' is not supported`);
+  }
 };
 
 export default getContent;
