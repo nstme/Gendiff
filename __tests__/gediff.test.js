e2c46fefdeece6edca33e78f84104e05b6c70ecd
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-05-27 23:24:09 -0700

    fix duplication in tests

--- __tests__/gediff.test.js
+++ __tests__/gediff.test.js
@@ -3,21 +3,17 @@ import fs from 'fs';
 import gendiff from '../src/index.js';
 
 const expected = fs.readFileSync('./__fixtures__/expected.txt', 'utf-8');
+const beforeJson = './__fixtures__/before.json';
+const afterJson = './__fixtures__/after.json';
+const beforeYml = './__fixtures__/before.yml';
+const afterYml = './__fixtures__/after.yml';
+const beforeIni = './__fixtures__/before.ini';
+const afterIni = './__fixtures__/after.ini';
 
-test('flat json', () => {
-  const beforeJson = './__fixtures__/before.json';
-  const afterJson = './__fixtures__/after.json';
-  expect(gendiff(beforeJson, afterJson)).toEqual(expected);
-});
-
-test('flat yaml', () => {
-  const beforeYaml = './__fixtures__/before.yml';
-  const afterYaml = './__fixtures__/after.yml';
-  expect(gendiff(beforeYaml, afterYaml)).toEqual(expected);
-});
-
-test('flat ini', () => {
-  const beforeIni = './__fixtures__/before.ini';
-  const afterIni = './__fixtures__/after.ini';
-  expect(gendiff(beforeIni, afterIni)).toEqual(expected);
+test.each([
+  [beforeJson, afterJson],
+  [beforeYml, afterYml],
+  [beforeIni, afterIni],
+])('gendiff', (before, after) => {
+  expect(gendiff(before, after)).toEqual(expected);
 });
