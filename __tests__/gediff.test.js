62c2fbddf073c6267d6e6fbdbe55e6f2ac7d73b2
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-06-15 20:43:09 -0700

    add test for deep json

--- __tests__/gediff.test.js
+++ __tests__/gediff.test.js
@@ -9,11 +9,14 @@ const beforeYml = './__fixtures__/before.yml';
 const afterYml = './__fixtures__/after.yml';
 const beforeIni = './__fixtures__/before.ini';
 const afterIni = './__fixtures__/after.ini';
+const beforeDeepJson = './__fixtures__/beforeDeep.json';
+const afterDeepJson = './__fixtures__/afterDeep.json';
 
 test.each([
   [beforeJson, afterJson],
   [beforeYml, afterYml],
   [beforeIni, afterIni],
+  [beforeDeepJson, afterDeepJson],
 ])('gendiff', (before, after) => {
   expect(gendiff(before, after)).toEqual(expected);
 });
